c6270d287a28cf623da836ad7f4068f8e841a10c
Sarah Murdock <sg.mdoc7@gmail.com>
2024-02-09 02:28:00 -0500

    start to navbar
    with cart icon and logo

---
+++ src/Components/Assets/all_product.js
@@ -0,0 +1,162 @@
+[
+    {
+        "id": 1,
+        "name": "Spiderman Sticker",
+        "category": "Superheroes",
+        "image": "p1_img",
+        "new_price": 0.05,
+        "old_price": 0.10
+    },
+    {
+        "id": 2,
+        "name": "Batman Logo Sticker",
+        "category": "Superheroes",
+        "image": "p2_img",
+        "new_price": 0.05,
+        "old_price": 0.10
+    },
+    {
+        "id": 3,
+        "name": "Wonder Woman Emblem",
+        "category": "Superheroes",
+        "image": "p3_img",
+        "new_price": 0.05,
+        "old_price": 0.10
+    },
+    {
+        "id": 4,
+        "name": "Cute Cat Sticker",
+        "category": "Animals",
+        "image": "p4_img",
+        "new_price": 0.04,
+        "old_price": 0.08
+    },
+    {
+        "id": 5,
+        "name": "Puppy Paw Print",
+        "category": "Animals",
+        "image": "p5_img",
+        "new_price": 0.04,
+        "old_price": 0.08
+    },
+    {
+        "id": 6,
+        "name": "Vintage Camera Sticker",
+        "category": "Hobbies",
+        "image": "p6_img",
+        "new_price": 0.06,
+        "old_price": 0.12
+    },
+    {
+        "id": 7,
+        "name": "Space Rocket Sticker",
+        "category": "Space",
+        "image": "p7_img",
+        "new_price": 0.07,
+        "old_price": 0.14
+    },
+    {
+        "id": 8,
+        "name": "Planet Earth Sticker",
+        "category": "Space",
+        "image": "p8_img",
+        "new_price": 0.07,
+        "old_price": 0.14
+    },
+    {
+        "id": 9,
+        "name": "Sunny Beach Sticker",
+        "category": "Nature",
+        "image": "p9_img",
+        "new_price": 0.03,
+        "old_price": 0.06
+    },
+    {
+        "id": 10,
+        "name": "Mountain Adventure Sticker",
+        "category": "Nature",
+        "image": "p10_img",
+        "new_price": 0.03,
+        "old_price": 0.06
+    },
+    {
+        "id": 11,
+        "name": "Gourmet Coffee Cup Sticker",
+        "category": "Food & Drink",
+        "image": "p11_img",
+        "new_price": 0.05,
+        "old_price": 0.10
+    },
+    {
+        "id": 12,
+        "name": "Sushi Roll Sticker",
+        "category": "Food & Drink",
+        "image": "p12_img",
+        "new_price": 0.05,
+        "old_price": 0.10
+    },
+    {
+        "id": 13,
+        "name": "Rainbow Pride Heart Sticker",
+        "category": "Pride",
+        "image": "p13_img",
+        "new_price": 0.04,
+        "old_price": 0.08
+    },
+    {
+        "id": 14,
+        "name": "Trans Pride Flag Sticker",
+        "category": "Pride",
+        "image": "p14_img",
+        "new_price": 0.04,
+        "old_price": 0.08
+    },
+    {
+        "id": 15,
+        "name": "Retro Vinyl Record Sticker",
+        "category": "Music",
+        "image": "p15_img",
+        "new_price": 0.06,
+        "old_price": 0.12
+    },
+    {
+        "id": 16,
+        "name": "Electric Guitar Sticker",
+        "category": "Music",
+        "image": "p16_img",
+        "new_price": 0.06,
+        "old_price": 0.12
+    },
+    {
+        "id": 17,
+        "name": "Funky Mushroom Sticker",
+        "category": "Nature",
+        "image": "p17_img",
+        "new_price": 0.03,
+        "old_price": 0.06
+    },
+    {
+        "id": 18,
+        "name": "Tropical Palm Leaves Sticker",
+        "category": "Nature",
+        "image": "p18_img",
+        "new_price": 0.03,
+        "old_price": 0.06
+    },
+    {
+        "id": 19,
+        "name": "Abstract Art Splash Sticker",
+        "category": "Art & Design",
+        "image": "p19_img",
+        "new_price": 0.07,
+        "old_price": 0.14
+    },
+    {
+        "id": 20,
+        "name": "Geometric Shapes Sticker",
+        "category": "Art & Design",
+        "image": "p20_img",
+        "new_price": 0.07,
+        "old_price": 0.14
+    }
+]
